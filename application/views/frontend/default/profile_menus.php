50ecec73bd4e0b11c3a5b3329e5e47a85007f3d9
kashifislam <kashif.islam@codeinformatics.com>
2022-01-04 11:45:49 +0500

    add changes

--- application/views/frontend/default/profile_menus.php
+++ application/views/frontend/default/profile_menus.php
@@ -14,7 +14,6 @@
 
                   <li class="<?php if($page_name == 'my_messages') echo 'active'; ?>"><a href="<?php echo site_url('home/my_messages'); ?>"><?php echo site_phrase('messages'); ?></a></li>
 
-                  <li class="<?php if($page_name == 'purchase_history' || $page_name == 'invoice') echo 'active'; ?>"><a href="<?php echo site_url('home/purchase_history'); ?>"><?php echo site_phrase('purchase_history'); ?></a></li>
 
                   <li class="<?php if($page_name == 'user_profile') echo 'active'; ?>"><a href="<?php echo site_url('home/profile/user_profile'); ?>"><?php echo site_phrase('profile'); ?></a></li>
                 </ul>
