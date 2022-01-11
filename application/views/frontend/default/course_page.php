fa851d5422805a7ff35ebbf0b600e7a9925f36bd
kashifislam <kashif.islam@codeinformatics.com>
2022-01-11 17:59:37 +0500

    --

--- application/views/frontend/default/course_page.php
+++ application/views/frontend/default/course_page.php
@@ -639,7 +639,7 @@ $instructor_details = $this->user_model->get_all_user($course_details['user_id']
                             <?php if (get_video_extension($course_details['video_url']) == 'mp4') : ?>
 
 <div class="player_timer_off" id="player">
-    <iframe height="500"
+    <iframe height="500" width="500"
         src="<?php echo $course_details['video_url']; ?>?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
         allowfullscreen allowtransparency allow="autoplay"></iframe>
 </div>
