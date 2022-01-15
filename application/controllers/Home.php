f0be540bea3648ca5e61561b99fe4fdd6c945bb6
kashifislam <kashif.islam@codeinformatics.com>
2022-01-15 17:06:06 +0500

    add changes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -117,7 +117,7 @@ class Home extends CI_Controller
             $config['base_url']  = site_url('home/courses?category='.$_GET['category'].'&&price=all&&level='.$selected_level.'&&language='.$selected_language.'&&rating='.$selected_rating);
             $this->pagination->initialize($config);
            
-            
+            if($total_rows > 0) {
                 $this->db->group_start();
                 $sale_ids_chunk = array_chunk($course_ids,25);
                 foreach($sale_ids_chunk as $sale_ids)
@@ -126,7 +126,7 @@ class Home extends CI_Controller
                 }
                 $this->db->group_end();
             
-            if($total_rows > 0) {
+           
             $page_data['courses'] =  $this->db->get('course',$config['per_page'], $this->input->get("per_page"))->result_array();
             } else {
                 $page_data['courses'] = 0 ;  
