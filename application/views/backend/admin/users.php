f6777c4fba8782762641905ded5a738fec6536ca
kashif islam <kashif.islam5620@gmail.com>
2022-01-18 14:33:14 +0500

    enrol user

--- application/views/backend/admin/users.php
+++ application/views/backend/admin/users.php
@@ -78,6 +78,7 @@
                                             <li><a class="dropdown-item"
                                                     href="<?php echo site_url('admin/user_form/edit_user_form/' . $user['id']) ?>"><?php echo get_phrase('edit'); ?></a>
                                             </li>
+                                           
                                         </ul>
                                     </div>
                                 </td>
