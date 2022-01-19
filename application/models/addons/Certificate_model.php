a7f6ad7421590b7cdc2792c449b3865af40bb5b4
kashifislam <kashif.islam@codeinformatics.com>
2022-01-19 23:31:54 +0500

    add changes

--- application/models/addons/Certificate_model.php
+++ application/models/addons/Certificate_model.php
@@ -105,7 +105,7 @@ class Certificate_model extends CI_Model
 		//echo BA.'system/fonts/'; exit;
 
 		for ($i=0; $i < $splited_certificate_template_part; $i++) {
-			$vrt_offset = 340;
+			$vrt_offset = 400;
 			$line_number = $i + 1;
 
 			// CONFIG CERTIFICATE TEMPLATE
@@ -121,8 +121,8 @@ class Certificate_model extends CI_Model
 			$config_certificate['wm_hor_alignment'] = 'center';
 			$config_certificate['wm_padding'] = '0';
 			$config_certificate['wm_hor_offset'] = '0';
-			$config_certificate['wm_vrt_offset'] = '400';
-			// $config_certificate['wm_vrt_offset'] = $vrt_offset + (40 * $i);
+			// $config_certificate['wm_vrt_offset'] = '400';
+			$config_certificate['wm_vrt_offset'] = $vrt_offset + (50 * $i);
 			$config_certificate['quality'] = '100%';
 
 		    //debug($splited_certificate_template[$i] , false);
