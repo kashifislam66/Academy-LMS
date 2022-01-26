c09eac6f887670f9cb9eb5fb9152d688edbf260a
kashif islam <kashif.islam5620@gmail.com>
2022-01-26 23:32:52 +0500

    mentanace file add

---
+++ application/views/maintenance.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Site Under Maintenance</title>
+    <link href="http://localhost/codeigniter/assets/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body class="bg">
+    <h1 class="head text-center">Site Under Maintenance</h1>
+    <div class="container">
+        <div class="content1"> 
+            <img src="https://goskillboost.com/uploads/system/logo-dark.png" alt="under-construction" width="120px" height="100px">
+            <p class="text-center">Sorry for the inconvenience. To improve our services, we have momentarily shutdown our site.</p>
+        </div>
+    </div>
+</body>
+</html>
+
+<style>
+  body{  text-align: center;
+    margin-top: 170px;
+  }
+</style>
